d6fc00d8ebe99057326b62958803b634aacf53f8
jiradawi <cutiemoon08@yahoo.com>
2025-01-10 21:55:40 +0700

    sort data by ENName

--- app/page.tsx
+++ app/page.tsx
@@ -56,7 +56,7 @@ export default function Home() {
             });
             return obj;
           })
-          .reverse();
+       //   .reverse();
 
         setShops(jsonData);
       } catch (error) {
